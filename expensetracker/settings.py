91f43bf36b0d7c57336b4c9e98833150672cd3e7
John Errico <john.errico@gmail.com>
2019-01-20 02:16:59 -0500

    django heroku

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ BOWER_INSTALLED_APPS = (
     'jquery-ui',
     'bootstrap',
 )
+
+django_heroku.settings(locals())
