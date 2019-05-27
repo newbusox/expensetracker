903666c201de1a4f8796a8a6e79863f044042427
John Errico <john.errico@gmail.com>
2019-05-27 10:20:19 -0400

    editing alcides util

--- expenses/utils.py
+++ expenses/utils.py
@@ -1,6 +1,10 @@
+import datetime
+
 from expenses.models import *
 
 def assign_alcides_to_work_day(date_start, date_end):
+    if abs((datetime.strptime(date_end, '%Y-%m-%d') - datetime.strptime(date_start, '%Y-%m-%d')).days) is not 14:
+        raise Exception('Date span must be 2 weeks')
     days = Day.objects.filter(date__gte=date_start, date__lte=date_end)
     alcides = Employee.objects.get(slug='alcides', foreman=True)
     for day in days:
@@ -12,6 +16,6 @@ def assign_alcides_to_work_day(date_start, date_end):
                 raise Exception('Already assigned')
             except:
                 work_day.employee.add(alcides)
-                calculated_pay = EmployeeCalculatedPay.objects.create(employee = alcides, amount = 1/total_work_days * alcides.daily_pay)
+                calculated_pay = EmployeeCalculatedPay.objects.create(employee = alcides, amount = 1/total_work_days * alcides.base_salary)
                 work_day.employee_calculated_salary.add(calculated_pay)
                 work_day.save()
