aed0b8574414c926c09eba8766533544aa9ab30e
John Errico <john.errico@gmail.com>
2019-05-29 22:38:45 -0400

    salaried bug fixes

--- expenses/utils.py
+++ expenses/utils.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from expenses.models import *
 
@@ -7,15 +7,19 @@ def assign_alcides_to_work_day(date_start, date_end):
         raise Exception('Date span must be 2 weeks')
     days = Day.objects.filter(date__gte=date_start, date__lte=date_end)
     alcides = Employee.objects.get(slug='alcides', foreman=True)
+    work_day_count = 0
     for day in days:
         work_days = day.workday_set.all()
-        total_work_days = len(work_days)
+        for work_day in work_days:
+            work_day_count += 1
+    for day in days:
+        work_days = day.workday_set.all()
         for work_day in work_days:
             try:
                 work_day.employee.get(slug=alcides.slug)
-                raise Exception('Already assigned')
+                pass
             except:
                 work_day.employee.add(alcides)
-                calculated_pay = EmployeeCalculatedPay.objects.create(employee = alcides, amount = 1/total_work_days * alcides.base_salary)
+                calculated_pay = EmployeeCalculatedPay.objects.create(employee = alcides, amount = 1/work_day_count * alcides.base_salary)
                 work_day.employee_calculated_salary.add(calculated_pay)
                 work_day.save()
